b122df9a0aa1f88b995e78c0109395f5157078f5
Alperen İNAL <inal.alperen@gmail.com>
2019-09-22 17:24:13 +0300

    amazon move zeros to end of array kata is added

---
+++ src/main/java/com/byinal/amcat/MoveZerosToEndOfArray.java
@@ -0,0 +1,39 @@
+package com.byinal.amcat;
+
+
+/*
+Given an array of random numbers,Push all the zero’s of a given array to the end of the array.
+For example, if the given arrays is {1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0}, it should be changed to {1, 9, 8, 4, 2, 7, 6, 0, 0, 0, 0}.
+The order of all other elements should be same. Expected time complexity is O(n) and extra space is O(1).
+ */
+public class MoveZerosToEndOfArray {
+
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0};
+        int[] solution = solution(numbers);
+        for (int s : solution) {
+            System.out.print(s + ", ");
+        }
+    }
+
+    private static int[] solution(int[] numbers) {
+        int nonZeroCount = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] != 0) {
+                numbers[nonZeroCount] = numbers[i];
+                nonZeroCount++;
+            }
+        }
+        for (int i = numbers.length - 1 ; i >= 0; i--) {
+            if (i >= nonZeroCount) {
+                numbers[i] = 0;
+            } else {
+                break;
+            }
+        }
+
+        return numbers;
+    }
+
+}
